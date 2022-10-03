a2c00d20336dbfaad4546ff1853b6b2f8bcfbeef
MassMurderer <jure.bagic@gmail.com>
2022-10-03 17:42:51 +0200

    Update bad_stack.rs
    Added helper function pop_node()

--- src/bad_stack.rs
+++ src/bad_stack.rs
@@ -1,50 +1,52 @@
 use std::mem;
 
 pub struct List {
-    head: Line
+    head: Link
 }
 
 impl List {
     pub fn new() -> Self {
-        List { head: Line::Empty }
+        List { head: Link::Empty }
     }
 
     pub fn push(&mut self, value: i32) {
         let new_node = Box::new(Node {
             element: value,
-            next: mem::replace(&mut self.head, Line::Empty)
+            next: self.pop_node()
         });
-        self.head = Line::Element(new_node);
+        self.head = Link::Element(new_node);
     }
     
     pub fn pop(&mut self) -> Option<i32> {
-        match mem::replace(&mut self.head, Line::Empty) {
-            Line::Empty => None,
-            Line::Element(node) => {
-                self.head = node.next;
-                Some(node.element)
-            }
+        if let Link::Element(node) = self.pop_node() {
+            self.head = node.next;
+            Some(node.element)
+        }else {
+            None
         }
+    }
+
+    fn pop_node(&mut self) -> Link {
+        mem::replace(&mut self.head, Link::Empty)
     }
 }
 
 impl Drop for List {
     fn drop(&mut self) {
-        let mut curr_link = mem::replace(&mut self.head, Line::Empty);
-        while let Line::Element(mut boxed_node) = curr_link {
-            curr_link = mem::replace(&mut boxed_node.next, Line::Empty);
+        while let Link::Element(boxed_node) = self.pop_node() {
+            self.head = boxed_node.next;
         }
     }
 }
 
-enum Line {
+enum Link {
     Empty,
     Element(Box<Node>)
 }
 
 struct Node {
     element: i32,
-    next: Line
+    next: Link
 }
 
 
@@ -73,6 +75,10 @@ mod test {
 
         // when list is empty
         assert_eq!(list.pop(), None);
+
+        list.push(2);
+        list.push(3);
+        list.push(4);
     }
 
 }
